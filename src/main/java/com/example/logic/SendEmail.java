e3dabe3e5315f19c351a94f8bc713c1b6458d0e0
minch13r <0414minyoung@naver.com>
2025-04-18 16:25:17 +0900

    이메일 API Legacy -> gradle dependency 사용 기존 jakarta와 javax 혼용 ->  javax로 통일 jakarata 사용시 다른 파일과 버전 일치 X

--- src/main/java/com/example/logic/SendEmail.java
+++ src/main/java/com/example/logic/SendEmail.java
@@ -1,4 +1,47 @@
 package com.example.logic;
 
+import javax.mail.*;
+import javax.mail.internet.AddressException;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+
+import java.util.Properties;
+
 public class SendEmail {
+    private static final String user = "mquetest@gmail.com"; // gmail 계정
+    private static final String password = "habaywbfkprzohor"; // gmail 패스워드
+
+    public static void sendMail(String toEmail, String subject, String content) {
+        Properties prop = new Properties();
+        prop.put("mail.smtp.auth", true);
+        prop.put("mail.smtp.starttls.enable", "true");
+        prop.put("mail.smtp.host", "smtp.gmail.com");
+        prop.put("mail.smtp.port", 587);
+        prop.put("mail.smtp.ssl.trust", "smtp.gmail.com");
+        prop.put("mail.smtp.ssl.protocols", "TLSv1.2");
+
+        Session session = Session.getDefaultInstance(prop, new Authenticator() {
+            @Override
+            protected PasswordAuthentication getPasswordAuthentication() {
+                return new PasswordAuthentication(user, password);
+            }
+        });
+
+        MimeMessage message = new MimeMessage(session);
+
+        try {
+            message.addRecipient(Message.RecipientType.TO, new InternetAddress(toEmail));
+            message.setSubject(subject);
+            message.setText(content);
+
+            // 이메일 송신
+            Transport.send(message);
+            System.out.println("이메일이 성공적으로 전송되었습니다.");
+
+        } catch (AddressException e) {
+            e.printStackTrace();
+        } catch (MessagingException e) {
+            e.printStackTrace();
+        }
+    }
 }
