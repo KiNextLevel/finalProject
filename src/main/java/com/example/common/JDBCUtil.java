262dec998da0c3fefd8b529e4c608b74e4d47804
유봉균 <bong990812@naver.com>
2025-04-18 15:51:48 +0900

    DAO 이관 중

--- src/main/java/com/example/common/JDBCUtil.java
+++ src/main/java/com/example/common/JDBCUtil.java
@@ -1,4 +1,45 @@
 package com.example.common;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
 public class JDBCUtil {
+
+    static final String driverName = "oracle.jdbc.driver.OracleDriver";
+    static final String url = "jdbc:oracle:thin:@localhost:1521:XE";
+
+    static final String userName = "root";
+    // db pw desktop은 030414, ki는 1234
+    static final String password = "1234";
+
+    public static Connection getConnection() {
+        Connection conn = null;
+        try {
+            // 1. 드라이버 연결(메모리에 데이터 적재)
+            Class.forName(driverName);
+            System.out.println("드라이버 연결 성공");
+            // 2. conn 연결
+            conn = DriverManager.getConnection(url, userName, password);
+            System.out.println("conn 연결 성공");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return conn;
+    }
+    public static void close(Connection conn, PreparedStatement pstmt) {
+        // 4. DB 연결 해제
+        try {
+            // null 체크 추가
+            if (pstmt != null) {
+                pstmt.close();
+            }
+            if (conn != null) {
+                conn.close();
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
 }
