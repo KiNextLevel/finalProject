7db1424cb41c664069250b1800fcea0fb4f74afc
유봉균 <bong990812@naver.com>
2025-04-18 12:04:00 +0900

    create VO

--- src/main/java/com/example/biz/preference/PreferenceService.java
+++ src/main/java/com/example/biz/preference/PreferenceService.java
@@ -1,4 +1,12 @@
 package com.example.biz.preference;
 
+import java.util.List;
+
 public interface PreferenceService {
+    boolean insert(PreferenceVO preferenceVO);
+    boolean update(PreferenceVO preferenceVO);
+    boolean delete(PreferenceVO preferenceVO);
+
+    PreferenceVO getMember(PreferenceVO preferenceVO);
+    List<PreferenceVO> getMemberList(PreferenceVO preferenceVO);
 }
--- src/main/java/com/example/biz/preference/impl/PreferenceServiceImpl.java
+++ src/main/java/com/example/biz/preference/impl/PreferenceServiceImpl.java
@@ -1,4 +1,39 @@
 package com.example.biz.preference.impl;
 
-public class PreferenceServiceImpl {
+import com.example.biz.preference.PreferenceService;
+import com.example.biz.preference.PreferenceVO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service("preferenceService")
+public class PreferenceServiceImpl implements PreferenceService {
+    @Autowired
+    private PreferenceDAO preferenceDAO;
+
+    @Override
+    public boolean insert(PreferenceVO preferenceVO) {
+        return preferenceDAO.insert(preferenceVO);
+    }
+
+    @Override
+    public boolean update(PreferenceVO preferenceVO) {
+        return preferenceDAO.update(preferenceVO);
+    }
+
+    @Override
+    public boolean delete(PreferenceVO preferenceVO) {
+        return preferenceDAO.delete(preferenceVO);
+    }
+
+    @Override
+    public PreferenceVO getPreference(PreferenceVO preferenceVO) {
+        return preferenceDAO.getMember(preferenceVO);
+    }
+
+    @Override
+    public List<PreferenceVO> getPreferenceList(PreferenceVO preferenceVO) {
+        return preferenceDAO.insert(preferenceVO);
+    }
 }
