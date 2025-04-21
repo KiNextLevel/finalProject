b1ab15b9a431f41b4ac24c9a75a9a06f2152d7b7
minch13r <0414minyoung@naver.com>
2025-04-21 15:58:16 +0900

    test commit

--- src/main/java/com/example/common/NextLevelApplication.java
+++ src/main/java/com/example/common/NextLevelApplication.java
@@ -2,8 +2,9 @@ package com.example.common;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
 public class NextLevelApplication {
 
 	public static void main(String[] args) {
