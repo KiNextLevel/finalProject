34d73b0907926e1e0afcbe8952cb5e233f0241e0
Ben <mque220@gmail.com>
2025-04-29 16:08:11 +0900

    VisitorDAO edited, a logic checking if the user logged in visited for the first time added.

---
+++ src/main/java/com/example/common/view/logic/CheckVisit.java
@@ -0,0 +1,29 @@
+package com.example.common.view.logic;
+
+import com.example.common.biz.user.UserVO;
+import com.example.common.biz.visitor.VisitorService;
+import com.example.common.biz.visitor.VisitorVO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component("checkVisit")
+public class CheckVisit {
+    @Autowired
+    private VisitorService visitorService;
+
+    public void checkVisitIfFirstLogin(UserVO userVO) {
+        System.out.println("checkVisitIfFirstLogin 로그 UserVO = " + userVO);
+        VisitorVO visitorVO = new VisitorVO();
+        visitorVO.setUserEmail(userVO.getUserEmail());
+        visitorVO.setCondition("GETONE");
+        // 방문자가 첫 방문일 경우
+        if (visitorService.getVisitor(visitorVO) == null) {
+            visitorService.insert(visitorVO);
+        }
+//        visitorVO.setCondition("GETONE_TODAY");
+//        System.out.println("visitor Td = " + visitorService.getVisitor(visitorVO));
+//        for (VisitorVO vo : visitorService.getVisitorList(visitorVO)) {
+//            System.out.println(vo);
+//        }
+    }
+}
