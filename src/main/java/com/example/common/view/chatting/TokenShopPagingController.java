1eac409b8b8a0e9158a59eb5d3052569d41ebcb4
NamYunJi <yjnam0123@gmail.com>
2025-05-06 16:13:10 +0900

    주석만 추가함

--- src/main/java/com/example/common/view/chatting/TokenShopPagingController.java
+++ src/main/java/com/example/common/view/chatting/TokenShopPagingController.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+// 만약, 토큰이 부족하면 토큰 상품 쇼핑으로 이동하기
 @Controller
 public class TokenShopPagingController {
     @GetMapping("/insufficientToken.do")
