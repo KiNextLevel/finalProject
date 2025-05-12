d562868af4086434488cf6e79a5cd21e219dbe48
NamYunJi <yjnam0123@gmail.com>
2025-05-12 16:24:56 +0900

    ChatMessageVO에 memberEmail2에 추가

--- src/main/java/com/example/common/biz/chatMessage2/ChatMessageVO.java
+++ src/main/java/com/example/common/biz/chatMessage2/ChatMessageVO.java
@@ -13,9 +13,11 @@ public class ChatMessageVO {
 
     private int messageId;          // 메시지 번호 (PK)
     private int chatRoomId;         // 채팅방 번호 (FK)
-    private String senderEmail;      // 발신자 이메일 (FK)
+    private String memberEmail1;      // 발신자 이메일 (FK)
+    private String memberEmail2;
     private String messageContent;   // 채팅 메시지 내용
     private Timestamp sentTime;      // 메시지 보낸 시각
+
     private String condition;        // 다양한 메서드를 위한 컨디션
     private String searchKeyword;
 
