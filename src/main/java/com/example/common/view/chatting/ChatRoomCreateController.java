23dd9a7cd3e5cdf0b53a8964c6003dc9db8ff5a9
NamYunJi <yjnam0123@gmail.com>
2025-05-12 16:26:16 +0900

    컨트롤러 파일 이름 변경(역할에 맞게), 주석 추가, 채팅방 생성해주는 컨트롤러

--- src/main/java/com/example/common/view/chatting/ChatRoomCreateController.java
+++ src/main/java/com/example/common/view/chatting/ChatRoomCreateController.java
@@ -14,31 +14,34 @@ import org.springframework.web.bind.annotation.RequestParam;
 // 만약 없다면 생성해주고 채팅방으로 입장,
 // 만약 있다면 바로 채팅방으로 입장
 @Controller
-public class ChatRoomController {
+public class ChatRoomCreateController {
     @Autowired
     private ChatRoomService chatRoomService;
 
     @GetMapping("/prepareChatRoom.do")
-    public String ChatRoomController(@RequestParam String targetEmail, HttpSession session, Model model, UserVO userVO, ChatRoomVO chatRoomVO) {
-       // 먼저 세션에서 내 이메일 꺼내오고
-        System.out.println( "채팅방 컨트롤러 로그 : " );
+    public String ChatRoom(@RequestParam String targetEmail, HttpSession session, ChatRoomVO chatRoomVO) {
+        System.out.println( "채팅방 컨트롤러 진입 성공 : ChatRoomController" );
+        //System.out.println("채팅방 번호 : " + chatRoomId);  // 채팅방 번호 받아오기 ( 채팅방 리스트에서)
+        // 먼저 세션에서 내 이메일 꺼내오고
         String myEmail = (String) session.getAttribute("userEmail");
         System.out.println("내 이메일 정보 : "+ myEmail);
 
         // 1. 디비에 채팅방이 이미 존재하는지 객체에 담아서 확인하기
-        chatRoomVO.setUser1Email(myEmail);
-        chatRoomVO.setUser2Email(targetEmail);
-        chatRoomVO.setCondition("SELECT_CHATROOM_BETWEEN_TWO_USERS");
+        chatRoomVO.setUser1Email(myEmail);  // 객체에 본인 이메일 넣기
+        chatRoomVO.setUser2Email(targetEmail); // 객체에 상대 이메일 넣기
+        chatRoomVO.setCondition("SELECT_CHATROOM_BETWEEN_TWO_USERS");  // 컨디션 설정한거 넣기
 
+        // DB에서 본인과 상대방 사이에 이미 존재하는 채팅방이 있는지 확인 (있으면 해당 방 정보를 반환)
         ChatRoomVO existingRoom = chatRoomService.getChatRoom(chatRoomVO);
-        System.out.println(existingRoom);
+        System.out.println("두 사람 사이에 방이 있니?(있으면 ChatRoomVO 객체 출력)  : " +existingRoom);
         int chatRoomId;
+        // 만약 방이 없다면?
         if (existingRoom == null) {
-            // 2. 없으면 새로 생성
-            chatRoomVO.setCondition("INSERT_CHAT_ROOM");
+            chatRoomVO.setCondition("INSERT_CHAT_ROOM");  // 컨디션 설정
             chatRoomService.insert(chatRoomVO);  // 이때 ID 생성됨
-            chatRoomId = chatRoomVO.getChatRoomId(); // 생성된 ID 가져오기
-        } else {
+            chatRoomId = chatRoomVO.getChatRoomId(); // DB에서 생성된 ID 가져오기
+        } // 만약 있다면?
+        else {
             chatRoomId = existingRoom.getChatRoomId(); // 기존 ID
             System.out.println("chatroom 아이디 이미 있을때 : " + chatRoomId);
         }
