2ecaac391968f96a44a20e298f9fba6145a913c5
NamYunJi <yjnam0123@gmail.com>
2025-05-12 16:31:31 +0900

    [refactoring] - 기존 코드에서 파일 분기

--- src/main/java/com/example/common/view/chatting/TokenDeductController.java
+++ src/main/java/com/example/common/view/chatting/TokenDeductController.java
@@ -1,5 +1,5 @@
 package com.example.common.view.chatting;
-//  채팅 시작 전, 토큰 1개를 차감하고 채팅방으로 이동하는 컨트롤러
+//  채팅 시작 전, 토큰 1개를 차감하는 컨트롤러
 //  로그인 사용자, 토큰, 입장 흐름 담당
 import com.example.common.biz.user.UserService;
 import com.example.common.biz.user.UserVO;
@@ -11,37 +11,13 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
-public class ChattingMoveController {
+public class TokenDeductController {
     @Autowired
     private UserService userService;
 
-    // 채팅 시작 전, 토큰 1개를 차감하고 채팅방으로 이동하는 컨트롤러
-//    @GetMapping("/deductToken.do")
-//    public String deductToken(HttpSession session, @RequestParam String targetEmail, UserVO userVO) {
-//        // 현재 로그인된 사용자의 이메일 가져오기 (세션에서 가져옴)
-//        String userEmail = (String) session.getAttribute("userEmail");
-//        System.out.println("세션에서 가져온 사용자 이메일: " + userEmail);
-//        System.out.println("요청받은 targetEmail: " + targetEmail);
-//
-//        // 로그인 상태일 경우에만 토큰 차감
-//        if (userEmail != null) {
-//            userVO.setUserEmail(userEmail);
-//            userVO.setCondition("UPDATE_MINUS_TOKEN");  // 토큰 1개 차감 쿼리
-//            System.out.println("토큰 차감 요청 전: " + userVO);
-//            // DB에 업데이트 요청
-//            userService.update(userVO);
-//            System.out.println("토큰 차감 완료");
-//        } else {
-//            System.out.println("세션에 로그인 정보가 없음 - 토큰 차감 생략");
-//        }
-//
-//        // 채팅방 페이지로 리다이렉트하면서 targetEmail을 파라미터로 전달
-//        // 다음 컨트롤러 메서드 (moveToChatRoom)에게 targetEmail 파라미터를 전달
-//        return "redirect:/chattingRoom.do?targetEmail=" + targetEmail;
-//        //return "redirect:/Metronic-Shop-UI-master/theme/WebSocket.jsp" + targetEmail;
-//    }
     @GetMapping("/deductToken.do")
     public String deductToken(HttpSession session, @RequestParam String targetEmail, Model model,  UserVO userVO) {
+        System.out.println( "토큰 차감하는 컨트롤러 진입 성공 : deductToken" );
         // 현재 로그인된 사용자의 이메일 가져오기 (세션에서 가져옴)
         String userEmail = (String) session.getAttribute("userEmail");
         System.out.println("세션에서 가져온 사용자 이메일: " + userEmail);
@@ -90,6 +66,33 @@ public class ChattingMoveController {
     }
 
 
+    // 채팅 시작 전, 토큰 1개를 차감하고 채팅방으로 이동하는 컨트롤러
+//    @GetMapping("/deductToken.do")
+//    public String deductToken(HttpSession session, @RequestParam String targetEmail, UserVO userVO) {
+//        // 현재 로그인된 사용자의 이메일 가져오기 (세션에서 가져옴)
+//        String userEmail = (String) session.getAttribute("userEmail");
+//        System.out.println("세션에서 가져온 사용자 이메일: " + userEmail);
+//        System.out.println("요청받은 targetEmail: " + targetEmail);
+//
+//        // 로그인 상태일 경우에만 토큰 차감
+//        if (userEmail != null) {
+//            userVO.setUserEmail(userEmail);
+//            userVO.setCondition("UPDATE_MINUS_TOKEN");  // 토큰 1개 차감 쿼리
+//            System.out.println("토큰 차감 요청 전: " + userVO);
+//            // DB에 업데이트 요청
+//            userService.update(userVO);
+//            System.out.println("토큰 차감 완료");
+//        } else {
+//            System.out.println("세션에 로그인 정보가 없음 - 토큰 차감 생략");
+//        }
+//
+//        // 채팅방 페이지로 리다이렉트하면서 targetEmail을 파라미터로 전달
+//        // 다음 컨트롤러 메서드 (moveToChatRoom)에게 targetEmail 파라미터를 전달
+//        return "redirect:/chattingRoom.do?targetEmail=" + targetEmail;
+//        //return "redirect:/Metronic-Shop-UI-master/theme/WebSocket.jsp" + targetEmail;
+//    }
+
+
 //    // 채팅방이동, 상대방 닉네임 jsp에 주는 컨트롤러
 //    @GetMapping("/chattingRoom.do")
 //    public String moveToChatRoom(@RequestParam String targetEmail ,Model model, UserVO userVO, HttpSession session ) {
@@ -121,29 +124,31 @@ public class ChattingMoveController {
 //    }
 //}
 
-    @GetMapping("/chattingRoom.do")
-    public String moveToChatRoom(@RequestParam String targetEmail,@RequestParam int chatRoomId, Model model, UserVO userVO, HttpSession session) {
-        System.out.println("targetEmail 상대방 이메일 확인 = " + targetEmail);
-        System.out.println("채팅방 ID: " + chatRoomId);   // 채팅방 ID 확인
-        // 상대방 정보 조회
-        userVO.setUserEmail(targetEmail);  // 객체에 상대방 이메일 넣기
-        userVO.setCondition("SELECTONE_USERINFO");  // 이메일을 통해 정보 조회 실시
-        UserVO targetUser = userService.getUser(userVO);  // 이메일 담아서 디비 실행해서 targetUser에 담기
-        System.out.println("상대방 정보 targetUser 내용: " + targetUser);
-        String targetNickname = targetUser.getUserNickname();  // 정보 가져온 것 중에 해당 이메일만 빼서 담기
-
-        // 내 닉네임을 세션이 아닌 DB에서 조회
-        String myEmail = (String) session.getAttribute("userEmail");  // 세션에서 이메일 가져오기
-        userVO.setUserEmail(myEmail); // 객체에 내 이메일 넣기
-        userVO.setCondition("SELECTONE_USERINFO");  // 정보 불러오는 쿼리문 실행
-        UserVO myNikname = userService.getUser(userVO);  // 이메일 담아서 디비 실행해서 myNikname에 담기
-        System.out.println("내 정보 myNikname 내용: " + myNikname);
-        String currentUserNickname = myNikname.getUserNickname();  // 불러온 정보 중에 닉네임만 빼서 currentUserNickname 담기
-
-        // 모델에 담기
-        model.addAttribute("chatRoomId", chatRoomId);  // 추가
-        model.addAttribute("targetNickname", targetNickname);  // 웹소켓 jsp로 상대방 닉네임 보내주기
-        model.addAttribute("currentUserNickname", currentUserNickname);  // 웹소켓 jsp로 본인 닉네임 보내주기
-        return "/Metronic-Shop-UI-master/theme/WebSocket";
-    }
+//    @GetMapping("/chattingRoom.do")
+//    public String moveToChatRoom(@RequestParam String targetEmail,@RequestParam int chatRoomId, Model model, UserVO userVO, HttpSession session) {
+//        System.out.println( "채팅방 이동하는 컨트롤러 진입 성공 : moveToChatRoom" );
+//        System.out.println("targetEmail 상대방 이메일 확인 = " + targetEmail);
+//        System.out.println("채팅방 ID: " + chatRoomId);   // 채팅방 ID 확인
+//
+//        // 상대방 정보 조회
+//        userVO.setUserEmail(targetEmail);  // 객체에 상대방 이메일 넣기
+//        userVO.setCondition("SELECTONE_USERINFO");  // 이메일을 통해 정보 조회 실시
+//        UserVO targetUser = userService.getUser(userVO);  // 이메일 담아서 디비 실행해서 targetUser에 담기
+//        System.out.println("상대방 정보 targetUser 내용: " + targetUser);
+//        String targetNickname = targetUser.getUserNickname();  // 정보 가져온 것 중에 해당 이메일만 빼서 담기
+//
+//        // 내 닉네임을 세션이 아닌 DB에서 조회
+//        String myEmail = (String) session.getAttribute("userEmail");  // 세션에서 이메일 가져오기
+//        userVO.setUserEmail(myEmail); // 객체에 내 이메일 넣기
+//        userVO.setCondition("SELECTONE_USERINFO");  // 정보 불러오는 쿼리문 실행
+//        UserVO myNikname = userService.getUser(userVO);  // 이메일 담아서 디비 실행해서 myNikname에 담기
+//        System.out.println("내 정보 myNikname 내용: " + myNikname);
+//        String currentUserNickname = myNikname.getUserNickname();  // 불러온 정보 중에 닉네임만 빼서 currentUserNickname 담기
+//
+//        // 모델에 담기
+//        model.addAttribute("chatRoomId", chatRoomId);  // 추가
+//        model.addAttribute("targetNickname", targetNickname);  // 웹소켓 jsp로 상대방 닉네임 보내주기
+//        model.addAttribute("currentUserNickname", currentUserNickname);  // 웹소켓 jsp로 본인 닉네임 보내주기
+//        return "/Metronic-Shop-UI-master/theme/WebSocket";
+//    }
 }
