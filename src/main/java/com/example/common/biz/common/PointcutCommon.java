cda7da7dd2cdf875db76a27f80045bdbc41614a9
Ben <mque220@gmail.com>
2025-05-12 16:25:18 +0900

    AOP 로깅용 포인트컷 설정

--- src/main/java/com/example/common/biz/common/PointcutCommon.java
+++ src/main/java/com/example/common/biz/common/PointcutCommon.java
@@ -1,12 +1,12 @@
-//package com.example.common.biz.common;
-//
-//import org.aspectj.lang.annotation.Aspect;
-//import org.aspectj.lang.annotation.Pointcut;
-//
-//@Aspect
-//public class PointcutCommon {
-//    @Pointcut("execution(* com.example.common.biz..*Impl.get*(..))")
-//    public void aPointcut() {}
-//    @Pointcut("execution(* com.example.common.biz..*Impl.*(..))")
-//    public void bPointcut() {}
-//}
+package com.example.common.biz.common;
+
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
+
+@Aspect
+public class PointcutCommon {
+    @Pointcut("execution(* com.example.common.biz..*Impl.get*(..))")
+    public void aPointcut() {}
+    @Pointcut("execution(* com.example.common.biz..*Impl.*(..))")
+    public void bPointcut() {}
+}
