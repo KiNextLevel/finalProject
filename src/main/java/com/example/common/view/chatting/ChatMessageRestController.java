45e30c45a4f0b82ad75349edb517f8d9880eeefb
NamYunJi <yjnam0123@gmail.com>
2025-05-12 16:24:29 +0900

    자신의 과거 채팅 리스트 불러와주는 컨트롤러 생성

--- src/main/java/com/example/common/view/chatting/ChatMessageRestController.java
+++ src/main/java/com/example/common/view/chatting/ChatMessageRestController.java
@@ -1,27 +1,27 @@
-//package com.example.common.view.chatting;
-//// 자신의 과거 채팅 리스트 불러오기
-//// 과거 메시지를 JSON으로 주는 API
-//import com.example.common.biz.chatMessage.ChatMessageService;
-//import com.example.common.biz.chatMessage.ChatMessageVO;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.web.bind.annotation.*;
-//
-//import java.util.List;
-//@RestController
-//public class ChatMessageRestController {
-//
-//    @Autowired
-//    private ChatMessageService chatMessageService;
-//
-//    // 과거 메시지 불러오기 (chatRoomId로)
-//    @GetMapping("/chatHistory.do")
-//    public List<ChatMessageVO> getChatHistory(@RequestParam Long chatRoomId,ChatMessageVO chatMessagevo) {
-//        chatMessagevo.setChatRoomId(chatRoomId);
-//        System.out.println("채팅메시지에서 채팅방 번호 가져오기 로그 : "+chatMessagevo.getChatRoomId());
-//
-//        return chatMessageService.getChatMessageList(chatMessagevo);
-//
-//    }
-//
-//}
-//
+package com.example.common.view.chatting;
+// 자신의 과거 채팅 리스트 불러오기
+// 과거 메시지를 JSON으로 주는 API
+
+import com.example.common.biz.chatMessage2.ChatMessageService;
+import com.example.common.biz.chatMessage2.ChatMessageVO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+public class ChatMessageRestController {
+
+    @Autowired
+    private ChatMessageService chatMessageService;
+
+    // 과거 메시지 불러오기 (chatRoomId로)
+    @GetMapping("/chat/messages")
+    public List<ChatMessageVO> getChatHistory(@RequestParam int chatRoomId, ChatMessageVO chatMessagevo) {
+        System.out.println(" 과거 채팅 내역 불러오는 컨트롤러 진입 : ChatMessageRestController ");
+        System.out.println(" 과거 메시지 불러오기: chatRoomId(채팅방 아이디 필요) = " + chatRoomId);
+        //chatMessagevo.setChatRoomId(chatRoomId);
+        return chatMessageService.getChatMessageList(chatMessagevo); // 오름차순 정렬된 메시지들
+    }
+}
+
