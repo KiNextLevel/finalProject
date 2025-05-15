446b273eba8d2846bd386d0fa8088b86934d9818
Ben <mque220@gmail.com>
2025-05-15 10:18:34 +0900

    엑셀 내보내기 csv파일에서 BOM 제거

--- src/main/java/com/example/common/view/excel/ExcelController.java
+++ src/main/java/com/example/common/view/excel/ExcelController.java
@@ -26,15 +26,8 @@ public class ExcelController {
             // 파일 이름 설정
             response.setHeader("Content-Disposition", "attachment;filename=paymentList.csv");
 
-            // Writer에 UTF-8 BOM 추가
-            OutputStream out = response.getOutputStream();
-            // UTF-8 전용 BOM 세팅
-            out.write(0xEF);
-            out.write(0xBB);
-            out.write(0xBF);
-
             // CSV 파일에 작성할 Writer
-            PrintWriter writer = new PrintWriter(new OutputStreamWriter(out, "UTF-8"));
+            PrintWriter writer = response.getWriter();
 
             // CSV 헤더 작성
             writer.println("결제 날짜,이메일,결제 금액,결제 상품");
@@ -60,11 +53,12 @@ public class ExcelController {
             // 읽어오기
             System.out.println("Excel로그: csv 읽어와서 깨지는거 없나 확인");
             try {
-                File file = new File("C:\\Users\\Administrator\\Downloads\\paymentList.csv");
+                File file = new File("C:\\Users\\Administrator\\Downloads\\paymentList (6).csv");
                 BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream(file), "UTF-8"));
                 String line;
                 String firstLine = br.readLine();
                 System.out.println("첫 줄 비교: " + firstLine.equals("결제 날짜,이메일,결제 금액,결제 상품"));
+                System.out.println("BOM출력 테스트: " + firstLine); // 스트링으로 나오나
                 System.out.println("첫 글자 코드: " + (int) firstLine.charAt(0)); // BOM이면 65279 출력
                 while ((line = br.readLine())!=null) {
                     System.out.println(line);
